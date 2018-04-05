6da23f988dff3eeb9c566ea991c382d4eb7cc9dd
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-04-05 05:34:13 +0200

    Remove dist files from tracking

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: (options?: {
-    callback?: ((...args: any[]) => any) | undefined;
-    offlineCallback?: ((...args: any[]) => any) | undefined;
-    interval?: number | undefined;
-    maxTries?: number | undefined;
-    navigator?: object | undefined;
-} | undefined) => Promise<{}>;
-export = _default;
